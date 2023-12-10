b488edb68e3c4570267f40c4b783b57cf9ca69a1
Enock <enocksm@outlook.com>
2023-12-10 17:49:08 -0300

    criando propria class pokemon e otimizado codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,16 @@
 
-
-function convertPokemonsTypesToLi(types) {
-    return types.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`).join('');
-
-}
-
-
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    ${convertPokemonsTypesToLi(pokemon.types)}
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                 </ol>
 
-                <img src="${pokemon.sprites.other.dream_world.front_default}" 
+                <img src="${pokemon.photo}" 
                     alt="${pokemon.name}">
             </div>
         </li>
@@ -26,7 +19,7 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons) => {
+pokeApi.getPokemons().then((pokemons = []) => {
     // mapeia o objeto e o transforma em um item de lista html
         const newHtml = pokemons.map(convertPokemonToLi).join('');
         pokemonList.innerHTML = newHtml;
